1b77cd80260b2ee03745ac4b7df14d3a2d8bde3a
Vinicius Lima <viniciusalmeidalima@icloud.com>
2025-11-17 09:11:44 -0300

    feat: Add porta para Frontend

--- app/main.py
+++ app/main.py
@@ -1,12 +1,26 @@
-# app/main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from app.api.cotacao_rotas import router as cotacao_router
+from app.api.cotacao_rotas import router as cotacao_router  # ou cotacao_routes
 
 app = FastAPI(
     title="Serviço de Cotação de Moedas",
     description="Serviço com cache em memória usando Frankfurter API.",
     version="0.2.0",
+)
+
+# 🔹 CORS: libera o front do Vite (porta 5173)
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(cotacao_router)
